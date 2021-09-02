6b5c318fbfb2e20bbe0fdfd03ee87573f8896488
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-09-02 16:40:26 +0530

    Add models for event app (#2)

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Time, Location, Category, Event
 
 # Register your models here.
+
+admin.site.register(Time)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Event)
