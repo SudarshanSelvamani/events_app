f141002d060466cee092d47cef75ed130bfeee86
Sudarshan <sudarshan@testpress.in>
2021-09-06 15:46:31 +0530

    add view for creating eve

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("events/create", views.EventCreateView.as_view(), name="create_event"),
     path("place/create", views.place_create_popup, name="create_place"),
     path("category/create", views.category_create_popup, name="create_category"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,11 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
 
-from .forms import EventPlaceForm, EventCategoryForm
+
+from .forms import EventForm, EventFormSet, EventPlaceForm, EventCategoryForm
+
+
+class EventCreateView(CreateView):
+    form_class = EventForm
+    template_name = "events/create_event.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EventCreateView, self).get_context_data(**kwargs)
+        context["event_formset"] = EventFormSet()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        event_formset = EventFormSet(
+            self.request.POST, self.request.FILES, instance=form.instance
+        )
+        print(event_formset.is_valid(), "is this true")
+        if form.is_valid() and event_formset.is_valid():
+            return self.form_valid(form, event_formset)
+        else:
+            return self.form_invalid(form, event_formset)
+
+    def form_valid(self, form, event_formset):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        event_times = event_formset.save()
+        return redirect("/")
+
+    def form_invalid(self, form, event_formset):
+        return self.render_to_response(
+            self.get_context_data(form=form, event_formset=event_formset)
+        )
 
 
 def place_create_popup(request):
-    form = EventPlaceForm(request.POST or None)
+    form = EventPlaceForm(request.POST)
     if form.is_valid():
         instance = form.save()
         return HttpResponse(
