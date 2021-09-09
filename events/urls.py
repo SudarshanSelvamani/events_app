2594f3e39c1285edefe8c04a982d89650e99f3ec
Sudarshan <sudarshan@testpress.in>
2021-09-09 13:48:47 +0530

    Add inline view for creating category in event create page

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("place/create", views.place_create_popup, name="create_place"),
+    path("category/create", views.category_create_popup, name="create_category"),
 ]
