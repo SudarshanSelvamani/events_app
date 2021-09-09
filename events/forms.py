9eca72ecdef77329aa01a35e7b77d48d3cd2a03b
Sudarshan <sudarshan@testpress.in>
2021-09-09 13:48:47 +0530

    Add inline view for creating category in event create page

--- events/forms.py
+++ events/forms.py
@@ -7,3 +7,9 @@ class EventPlaceForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = "__all__"
+
+
+class EventCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = "__all__"
