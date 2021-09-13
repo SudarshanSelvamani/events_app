47c33eee173839b3e6da6ae133a8cc9ab240992d
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-09-13 11:48:12 +0530

    Add inline view for creating category in event create page (#7)

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import EventPlaceForm
+from .forms import EventPlaceForm, EventCategoryForm
 
 
 def place_create_popup(request):
@@ -14,3 +14,15 @@ def place_create_popup(request):
         )
     else:
         return render(request, "events/create_event_place.html", {"form": form})
+
+
+def category_create_popup(request):
+    form = EventCategoryForm(request.POST)
+    if form.is_valid():
+        instance = form.save()
+        return HttpResponse(
+            '<script>opener.closePopup(window, "%s", "%s", "#id_category");</script>'
+            % (instance.pk, instance)
+        )
+    else:
+        return render(request, "events/create_category.html", {"form": form})
