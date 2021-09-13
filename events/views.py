5ebcfa26f3135b1891e4c73a2c5bbc70fcbf95d3
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-09-13 21:43:40 +0530

    Add view for creating events (#3)

--- events/views.py
+++ events/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 
-from .forms import EventPlaceForm, EventCategoryForm
+from .forms import EventPlaceForm, EventCategoryForm, EventFormSet, EventForm
+
+
+class EventCreateView(CreateView):
+    form_class = EventForm
+    template_name = "events/create_event.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EventCreateView, self).get_context_data(**kwargs)
+        context["event_formset"] = EventFormSet()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        event_formset = EventFormSet(
+            self.request.POST, self.request.FILES, instance=form.instance
+        )
+        if form.is_valid() and event_formset.is_valid():
+            return self.form_valid(form, event_formset)
+        else:
+            return self.form_invalid(form, event_formset)
+
+    def form_valid(self, form, event_formset):
+        object = form.save(commit=False)
+        object.user = self.request.user
+        object.save()
+        event_times = event_formset.save()
+        return redirect("/")
+
+    def form_invalid(self, form, event_formset):
+        return self.render_to_response(
+            self.get_context_data(form=form, event_formset=event_formset)
+        )
 
 
 def place_create_popup(request):
